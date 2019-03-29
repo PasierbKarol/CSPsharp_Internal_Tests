d00d08d94490c0022b93a15096d1098a53bc0282
pasierbkarol <pasierbkarol@gmail.com>
2019-03-29 11:48:54 +0000

    Added testing projects from the main library

---
+++ CSPsharp_Internal_Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSPsharp_Internal_Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
